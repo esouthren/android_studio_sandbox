40477ad11c7c724cb9c3f3a66309bc7d2ff3f57a
esouthren <eilidhsouthren@gmail.com>
2018-11-14 13:44:38 +0000

    removed default toolbar

--- 1513195_coursework/app/src/main/java/com/example/eilidh/a1513195_coursework/MainActivity.kt
+++ 1513195_coursework/app/src/main/java/com/example/eilidh/a1513195_coursework/MainActivity.kt
@@ -5,6 +5,8 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.view.Window
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -32,5 +34,9 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    fun callApi(view: View) {
+        // api gets called here
     }
 }
