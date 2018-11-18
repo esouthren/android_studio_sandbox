c7cafab7110821568829ffae3155c3f796b04f38
esouthren <eilidhsouthren@gmail.com>
2018-11-18 21:58:34 +0000

    Can add up to 10 preferences, messy but functional

--- 1513195_coursework/app/src/main/java/com/example/eilidh/a1513195_coursework/UserPreferences.kt
+++ 1513195_coursework/app/src/main/java/com/example/eilidh/a1513195_coursework/UserPreferences.kt
@@ -16,28 +16,48 @@ import android.content.Context.MODE_PRIVATE
 import android.content.SharedPreferences
 import android.graphics.Color
 import android.util.Log
+import android.widget.EditText
 import android.widget.TextView
+import com.example.eilidh.a1513195_coursework.R.id.none
+
+
 
 class Prefs (context: Context) {
     val PREFS_FILENAME = "user_preferences"
     // array of 0 - 9 to store user preferences
     val PREFERENCES = Array(10, { i -> (i + 1).toString() })
-    val prefs: SharedPreferences = context.getSharedPreferences(PREFS_FILENAME, 0);
+
+    val preferences: SharedPreferences = context.getSharedPreferences(PREFS_FILENAME, 0);
 
     fun setPref(index: Int, value: String) {
-        prefs.edit().putString(PREFERENCES[index], value).apply()
+        this.preferences.edit().putString(PREFERENCES[index], value).apply()
     }
 
     fun getPref(index: Int): String {
-        return prefs.getString(PREFERENCES[index], "huh?")
+        return this.preferences.getString(PREFERENCES[index], "")
+
     }
     fun clear() {
-        prefs.edit().clear()
+        for(i in 0..9) {
+            this.preferences.edit().putString(PREFERENCES[i], " ").apply()
+        }
         // add 'preferences cleared' toast?
     }
+
 }
 
 class UserPreferences : AppCompatActivity() {
+
+    var editTextIds = arrayOf(R.id.user_pref_1,
+            R.id.user_pref_2,
+            R.id.user_pref_3,
+            R.id.user_pref_4,
+            R.id.user_pref_5,
+            R.id.user_pref_6,
+            R.id.user_pref_7,
+            R.id.user_pref_8,
+            R.id.user_pref_9,
+            R.id.user_pref_10)
 
     var prefs: Prefs? = null
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,28 +65,81 @@ class UserPreferences : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.user_preferences)
         prefs = Prefs(this)
-        var userPrefTextView = arrayOfNulls<String>(10)
         displayUserPreferences()
-
-        // create text view references
-        for (i in 0..9) {
-            userPrefTextView[i] = "R.id.user_pref_" + i.toString()
-        }
     }
 
     fun displayUserPreferences() {
         // load user preferences and display
-        val textView: TextView = findViewById(R.id.user_pref_1) as TextView
-        textView.text = prefs!!.getPref(0)
-        Log.i("debug", "displaying user preferences" )
+        var boxIndex = 0
 
+
+        for(prefIndex in 0..9) {
+            findViewById<EditText>(editTextIds[prefIndex]).visibility = View.INVISIBLE
+            Log.i("debug", "pref " + prefIndex + ": " + prefs!!.getPref(prefIndex).length.toString() )
+            val editText: EditText = findViewById<EditText>(editTextIds[boxIndex])
+            editText.setText(prefs?.getPref(prefIndex))
+            if(prefs!!.getPref(prefIndex).length > 1) {
+                //Log.i("debug", "contains text!" + prefs!!.getPref(i))
+                editText.setText(prefs!!.getPref(prefIndex))
+                Log.i("debug", "setting visible")
+                editText.visibility = View.VISIBLE
+                boxIndex++
+            }
+
+
+        }
+        // make box 1 visible always
+        findViewById<EditText>(editTextIds[0]).visibility = View.VISIBLE
+
+
+
+        //editText.text = prefs!!.getPref(0)
+        Log.i("debug", "displaying user preferences" )
+        //removeUnusedEditTexts()
 
     }
 
     fun updateUserPreferences(view: View) {
-        val b: TextView = findViewById(R.id.user_pref_1) as TextView
-        prefs!!.setPref(1, b.text.toString())
+
+
+        for(editIndex in 0..9) {
+            val editText: EditText = findViewById<EditText>(editTextIds[editIndex])
+            if (editText.text.toString().length > 1) {
+                Log.i("debug", "value of textbox: " + editText.text.toString().length.toString())
+                prefs?.setPref(editIndex, editText.text.toString())
+                Log.i("debug", "setting preference: " + editIndex + " - " + prefs!!.getPref(editIndex))
+            }
+        }
         displayUserPreferences()
-        Log.i("debug", prefs!!.getPref(1))
+
+    }
+
+
+
+    fun deletePreference() {
+        // update the order of preferences if , say, 2nd preference of 3 is deleted
+
+    }
+
+    fun addPreferenceEditBox(view: View) {
+        updateUserPreferences(view)
+        for (i in 0..9) {
+            val editText: EditText = findViewById<EditText>(editTextIds[i])
+                if (prefs!!.getPref(i).length <= 1) {
+                    editText.visibility = View.VISIBLE
+                    break
+                }
+            }
+    }
+
+    fun deleteAllPreferences(view: View) {
+        Log.i("debug", "Deleting all preferences")
+        prefs!!.clear()
+        for(i in 0..9) {
+            val editText: EditText = findViewById<EditText>(editTextIds[i])
+            editText.setText("")
+            Log.i("debug", "deleted pref: " + prefs!!.getPref(i))
+        }
+        displayUserPreferences()
     }
 }
