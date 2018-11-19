b3161f6451743b878b4f3093a9853f9feb45c7ba
esouthren <eilidhsouthren@gmail.com>
2018-11-19 16:45:02 +0000

    Can add/clear all database data

--- 1513195_coursework/app/src/main/java/com/example/eilidh/a1513195_coursework/FillDatabase.kt
+++ 1513195_coursework/app/src/main/java/com/example/eilidh/a1513195_coursework/FillDatabase.kt
@@ -9,6 +9,7 @@ import android.util.Log
 import android.widget.Toast
 import com.google.gson.Gson
 import org.json.JSONObject
+import java.util.*
 
 class FillDatabase(mDb: WeatherDatabase, mDbWorkerThread: DbWorkerThread) {
 
@@ -17,65 +18,73 @@ class FillDatabase(mDb: WeatherDatabase, mDbWorkerThread: DbWorkerThread) {
 
 
     fun addDataToDatabase(data: ApiData.CoreData, context: Context) {
+        // clear database
+        clearDatabase()
+        Log.i("debug", "adding data to database")
         fetchWeatherDataFromDb(context)
         /*
-        Log.i("debug", "hello!")
-        mDbWorkerThread = DbWorkerThread("dbWorkerThread")
 
-        //var gson = Gson()
-        //var jsonData = gson.fromJson(data.toString(), ApiData.CoreData::class.java)
-        //var weatherData = WeatherData()
 
         //val task = Runnable { db?.userDao()?.insert(weatherData) }
         //thread.postTask(task)
 
         // todo: add internet connectivity check
         */
-        /*val weatherData = WeatherData(10,"",0.0,0.0,0,
-                "","","",0.0, 0.0,
+        val bloop = Random().nextInt((100000000 + 1) - 1) + 1
+
+        val weatherData = WeatherData(bloop, "holla!", 0.0, 0.0, 0,
+                "", "", "", 0.0, 0.0,
                 "", "", 0.0, 0.0,
-                0.0,0.0,0.0,0.0,0.0)
-*/
-        //insertWeatherDataInDb(weatherData)
+                0.0, 0.0, 0.0, 0.0, 0.0)
+
+        insertWeatherDataInDb(weatherData)
+        fetchWeatherDataFromDb(context)
 
 
-}
+    }
 
-private fun fetchWeatherDataFromDb(context: Context) {
+    private fun fetchWeatherDataFromDb(context: Context) {
 
 
-    val task = Runnable {
-        val weatherData =
-                mDb?.weatherDao()?.getAll()
+        val task = Runnable {
+            val weatherData =
+                    mDb?.weatherDao()?.getAll()
 
             if (weatherData == null || weatherData?.size == 0) {
                 Log.i("debug", "no data in database :(")
             } else {
-                Log.i("debug", "there's some data!" + weatherData[0])
+                Log.i("debug", "there's some data!" + weatherData.size)
 
             }
+        }
+        mDbWorkerThread.postTask(task)
+
     }
-    mDbWorkerThread.postTask(task)
 
-}
+    private fun insertWeatherDataInDb(weatherData: WeatherData) {
+        Log.i("debug", "Inserting data...")
+        val task = Runnable { mDb?.weatherDao()?.insert(weatherData) }
+        mDbWorkerThread.postTask(task)
+    }
 
-private fun insertWeatherDataInDb(weatherData: WeatherData) {
-    Log.i("debug", "Inserting data...")
-    val task = Runnable { mDb?.weatherDao()?.insert(weatherData) }
-    mDbWorkerThread.postTask(task)
-}
-
-fun onDestroy() {
-    WeatherDatabase.destroyInstance()
-    mDbWorkerThread.quit()
-    //super.onDestroy()
-}
+    fun onDestroy() {
+        WeatherDatabase.destroyInstance()
+        mDbWorkerThread.quit()
+        //super.onDestroy()
+    }
 
 
-fun isConnectedToInternet(context: Context): Boolean {
+    fun isConnectedToInternet(context: Context): Boolean {
         val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         var activeNetworkInfo: NetworkInfo? = null
         activeNetworkInfo = cm.activeNetworkInfo
         return activeNetworkInfo != null && activeNetworkInfo.isConnectedOrConnecting
     }
+
+
+    private fun clearDatabase() {
+        val task = Runnable { mDb?.weatherDao()?.deleteAll() }
+        mDbWorkerThread.postTask(task)
+    }
+
 }
