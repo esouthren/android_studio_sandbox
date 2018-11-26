07077da8541c82f161d1cc229cbee1b276379d90
esouthren <eilidhsouthren@gmail.com>
2018-11-26 15:24:45 +0000

    search page skeleton created

--- 1513195_coursework/app/src/main/java/com/example/eilidh/a1513195_coursework/SearchWeather.kt
+++ 1513195_coursework/app/src/main/java/com/example/eilidh/a1513195_coursework/SearchWeather.kt
@@ -2,6 +2,8 @@ package com.example.eilidh.a1513195_coursework
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import javax.security.auth.callback.Callback
 
 class SearchWeather : AppCompatActivity(), Callback {
@@ -10,5 +12,18 @@ class SearchWeather : AppCompatActivity(), Callback {
         setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_weather)
+
+        val spinner: Spinner = findViewById(R.id.search_spinner)
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+                this,
+                R.array.search_weather_array,
+                android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
+        }
     }
 }
